147817366ea18de09805ee617dc7758719276c4b
Lucas Araujo <lucas.araujo@solutis.net.br>
2019-07-24 16:03:07 -0300

    Apenas consegui completar o código após assistir a aula do Guanabara

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,15 +1,34 @@
-var inicio = document.querySelector('input#inicio')
-var fim = document.querySelector('input#fim')
-var passo = document.querySelector('input#passo')
-var res = document.querySelector('div#res')
-// var contador = inicio.value
 function contar(){
-    if(inicio.value == '' || fim.value == '' || passo.value == ''){
-        alert('Por favor, insira um valor no campo inicio!')
-    }
-    else{
-        while(inicio <= fim){
-            res.innerHTML += inicio + passo
+    var inicio = document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.querySelector('input#passo')
+    var res = document.querySelector('div#res')
+        if(inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+           alert('Por favor, insira um valor no campo inicio!')
+       
+        }else {
+            res.innerHTML = 'Contando...'
+            var i = Number(inicio.value)
+            var f = Number(fim.value)
+            var p = Number(passo.value)
+            if (p <= 0) {
+                alert('Passo Invalido! Considerando Passo como 1')
+                p = 1
+            }
+           
+            if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+                
+        }else if(i > f){
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-    }
+           
+           
+        
+         }
+         res.innerHTML += `\u{1F3C6}`
 }
