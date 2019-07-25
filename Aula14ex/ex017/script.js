c740e9120717df237dd194067e3f7fdb0132d86f
Lucas Araujo <lucas.araujo@solutis.net.br>
2019-07-25 17:14:59 -0300

    Nesse momento segui a aula de resolução do Guanabara para no proximo projeto tentar reproduzir só

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function validar(){
+    let numero = document.getElementById('numero')
+    let tab = document.getElementById('tab')
+    if (numero.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    } else{
+        let num = Number(numero.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+  
+}
