0f15a5cbbe1f8e89cd740556ad778ce4cab3b922
Beni <beninigmal@gmail.com>
2019-07-30 14:01:33 -0300

    Arquivos excedentes e desatualizados.

--- EstudosJS/Aula 11/ex009.js
+++
@@ -1,9 +0,0 @@
-// Condição composta
-var pais = 'Brasil'
-console.log(`Se você vive no ${pais}, então `)
-if(pais == 'Brasil'){
-    console.log(`Você é brasileiro`)
-}
-else{
-    console.log('Você é estrangeiro!')
-}
