4ba0fff3b517cc50b9da900cf1314b9747ba9498
Lucas Araujo <lucas.araujo@solutis.net.br>
2019-08-01 15:06:44 -0300

    Continuado o que comecei em casa e concluído usando apoio de Yuri com essa lógica.

--- Repetição/Repetição ex016.2/script.js
+++ Repetição/Repetição ex016.2/script.js
@@ -3,42 +3,43 @@ function contar() {
   let fim = document.getElementById('fim')
   let passo = document.getElementById('passo')
   let res = document.querySelector('div#res')
+  res.innerHTML = null
 
   if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     alert('Preencha os campos obrigatórios!')
-  }
-  else if (inicio.value == fim.value) {
+  } else if (inicio.value == fim.value) {
     alert('O campo INICIO não pode ser igual ao campo FIM!')
-  }
-  if (passo.value == 0) {
-    alert('Não é possível efetuar a contagem com o valor 0, será considerado o valor 1')
-    passo = 1
-
-  }
-  else {
+  } else if (Number(passo.value) <= 0) {
     ini = Number(inicio.value)
     f = Number(fim.value)
     p = Number(passo.value)
     res.innerHTML += 'Contando:'
-
+    alert('Não é possível efetuar a contagem com o valor 0, será considerado o valor 1')
+    p = 1
     if (ini < f) {
-      for (let i = ini; i < f; i += p) {
+      for (let i = ini; i <= f; i += p) {
         res.innerHTML += ` ${i} \u{27a1}`
-
       }
       res.innerHTML += ` \u{2705}`
-    }else{
-      for (let i = ini; i > f; i -= p) {
+    } else {
+      for (let i = ini; i >= f; i -= p) {
         res.innerHTML += ` ${i} \u{27a1}`
-        
       }
       res.innerHTML += ` \u{2705}`
     }
 
 
-
-
-
   }
+
+
+
+
+
+
+
+
+
+
+
 
 }
