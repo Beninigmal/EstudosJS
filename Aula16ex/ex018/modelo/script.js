3f75c486ab99cdfbac62224bde18cd9551e6da7e
Lucas Araujo <lucas.araujo@solutis.net.br>
2019-08-06 18:45:43 -0300

    Vários exercicios e o inicio do exercicio, porém sem exito, terminei me batendo na verificação do select e option

---
+++ Aula16ex/ex018/modelo/script.js
@@ -0,0 +1,13 @@
+function adicionar() {
+    let numero = document.getElementById('numero')
+    let exibir = document.querySelector('select#varicom')
+
+    exibir.innerHTML = 'numero'
+
+    // let cal = document.createElement('option')
+    // cal.text = `O valor digitado foi ${numero}`
+    // cal.value = `exibir${numero}`
+
+    // exibir.appendChild(cal)
+
+}
