d68b4fb549c172990946d73034ccb5286c147291
Lucas Araujo <lucas.araujo@solutis.net.br>
2019-08-07 19:02:31 -0300

    Hoje básicamente serviu para eu entender como funciona melhor os parametros das funções... continuarei com o vídeo de resolução, mas antes quebrarei um pouco mais a cabeça tentando fazer do meu jeito.

--- Aula16ex/ex018/modelo/script.js
+++ Aula16ex/ex018/modelo/script.js
@@ -1,13 +1,39 @@
+// let numero = document.getElementById('numero')
+// let exibir = document.querySelector('select#varicom')
+// let res = document.querySelector('div#res')
+// let valores = []
+
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
 function adicionar() {
     let numero = document.getElementById('numero')
     let exibir = document.querySelector('select#varicom')
+    let res = document.querySelector('div#res')
+    let valores = []
 
-    exibir.innerHTML = 'numero'
 
-    // let cal = document.createElement('option')
-    // cal.text = `O valor digitado foi ${numero}`
-    // cal.value = `exibir${numero}`
+    if (isNumero(numero.value) && !inLista(numero.value, valores)) {
+        let item = document.createElement('option')
+        item.text = `Inserido o número: ${numero.value}`
+        item.value = `${numero.value}`
+        exibir.appendChild(item)
 
-    // exibir.appendChild(cal)
-
+    } else {
+        alert('Valor invalido ou já encontrado na lista')
+    }
 }
--- Repetição/Repetição ex017/script.js
+++ Repetição/Repetição ex017/script.js
@@ -1,19 +1,19 @@
 function validar() {
     let numero = document.getElementById('numero')
     let tab = document.getElementById('tab')
-    if(numero.value.length < 1 || numero.value.length > 1){
+    if (numero.value.length < 1 || numero.value.length > 1) {
         alert('Digite um número de 1 a 9!')
-    }else if (numero.value == 0) {
+    } else if (numero.value == 0) {
         alert('Todos os números multiplicados por 0 o resultado será 0, insira um valor de 1 a 9!')
     } else {
         let num = Number(numero.value)
         tab.innerHTML = ''
-        for (let c = 1; c <= 10; c++){
+        for (let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${num} x ${c} = ${num*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
-1           
+            1
         }
     }
-   }
+}
