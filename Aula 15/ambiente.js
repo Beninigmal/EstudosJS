c6dbd3d780a2c2d71b5c3f7c94cddda1628f993e
Beni <beninigmal@gmail.com>
2019-08-08 23:42:48 -0300

    Exercicio completo

---
+++ Aula 15/ambiente.js
@@ -0,0 +1,16 @@
+let num = [1,2]
+num.sort
+num.push(7,9,12)
+
+
+
+console.log(`O vetor tem ${num.length} posições com os números ${num}`)
+
+// for (let pos = 0; pos<num.length ; pos++) {
+//     console.log(num[pos])
+    
+// }
+// for (let pos in num) {
+//    console.log(`A posição ${pos} tem os valores ${num[pos]}`)
+// }
+console.log(num.indexOf(12))
